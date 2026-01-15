6a2fafe5936ee073b99804d0f8a351be6494c6bf
mmrdr <isaev2k18@gmail.com>
2026-01-15 13:53:58 +0300

    Created interface for doorman api service

--- edium/lib/services/doorman_api_service/doorman_api_service.dart
+++ edium/lib/services/doorman_api_service/doorman_api_service.dart
@@ -1,11 +1,13 @@
+import 'package:edium/services/doorman_api_service/doorman_api_service_interface.dart';
 import 'package:edium/services/doorman_api_service/doorman_dto.dart';
 import 'package:edium/services/network/base_api_service.dart';
 import 'package:edium/services/network/endpoints.dart';
 import 'package:edium/services/network/http_method.dart';
 
-class DoormanApiService extends BaseApiService {
+class DoormanApiService extends BaseApiService implements IDoormanApiService {
   DoormanApiService(super.dio);
 
+  @override
   Future<void> sendOtpRequest(OtpSendRequest req) async {
     return request(
       DoormanEndpoints.otpSend.path,
@@ -16,6 +18,7 @@ class DoormanApiService extends BaseApiService {
     );
   }
 
+  @override
   Future<AuthTokensResponse> otpVerifyRequest(OtpVerifyRequest req) async {
     return request(
       DoormanEndpoints.otpVerify.path,
@@ -25,7 +28,8 @@ class DoormanApiService extends BaseApiService {
       parser: (data) => AuthTokensResponse.fromJson(data)
     );
   }
-
+  
+  @override
   Future<AuthTokensResponse> registerRequest(RegisterRequest req) async {
     return request(
       DoormanEndpoints.authRegister.path,
@@ -36,6 +40,7 @@ class DoormanApiService extends BaseApiService {
     );
   }
 
+  @override
   Future<AuthTokensResponse> refreshTokensRequest(RefreshTokenRequest req) async {
     return request(
       DoormanEndpoints.authTokensRefresh.path,
@@ -46,6 +51,7 @@ class DoormanApiService extends BaseApiService {
     );
   }
 
+  @override
   Future<void> logoutRequest() async {
     return request(
       DoormanEndpoints.authLogout.path,
