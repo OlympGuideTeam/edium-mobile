71179e870fb8b1e8ff9ab7465a4012a86dfb1a0f
mmrdr <kialisaev@edu.hse.ru>
2026-04-12 14:52:45 +0300

    Редизайн библиотеки квизов

--- edium/lib/presentation/shared/widgets/search_bar_widget.dart
+++ edium/lib/presentation/shared/widgets/search_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:edium/core/theme/app_colors.dart';
+import 'package:edium/core/theme/app_dimens.dart';
 import 'package:edium/core/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
@@ -20,35 +21,51 @@ class SearchBarWidget extends StatefulWidget {
 
 class _SearchBarWidgetState extends State<SearchBarWidget> {
   final _controller = TextEditingController();
+  final _focusNode = FocusNode();
+  bool _focused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      if (_focusNode.hasFocus != _focused) {
+        setState(() => _focused = _focusNode.hasFocus);
+      }
+    });
+  }
 
   @override
   void dispose() {
     _controller.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 44,
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      height: AppDimens.inputH,
       decoration: BoxDecoration(
-        color: AppColors.surface,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: AppColors.cardBorder),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(AppDimens.radiusMd),
+        border: Border.all(
+          color: _focused ? AppColors.mono700 : AppColors.mono200,
+          width: AppDimens.borderWidth,
+        ),
       ),
       child: TextField(
         controller: _controller,
+        focusNode: _focusNode,
         onChanged: (v) {
           setState(() {});
           widget.onChanged(v);
         },
-        style: AppTextStyles.bodySmall,
+        style: AppTextStyles.fieldText,
         decoration: InputDecoration(
           hintText: widget.hint,
-          hintStyle:
-              AppTextStyles.bodySmall.copyWith(color: AppColors.textSecondary),
-          prefixIcon: const Icon(Icons.search, size: 18,
-              color: AppColors.textSecondary),
+          hintStyle: AppTextStyles.fieldHint,
+          prefixIcon: Icon(Icons.search, size: 20, color: AppColors.mono300),
           suffixIcon: _controller.text.isNotEmpty
               ? GestureDetector(
                   onTap: () {
@@ -57,15 +74,14 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
                     widget.onClear?.call();
                     setState(() {});
                   },
-                  child: const Icon(Icons.clear, size: 18,
-                      color: AppColors.textSecondary),
+                  child: Icon(Icons.clear, size: 18, color: AppColors.mono300),
                 )
               : null,
           border: InputBorder.none,
           enabledBorder: InputBorder.none,
           focusedBorder: InputBorder.none,
           contentPadding:
-              const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+              const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
           isDense: true,
         ),
       ),
