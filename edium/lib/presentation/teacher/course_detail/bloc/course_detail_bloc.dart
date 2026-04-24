31a735873ffd30aedf4440b2895b624e387afb56
mmrdr <kialisaev@edu.hse.ru>
2026-04-24 11:28:22 +0300

    feat: add client-side role guard to CourseDetailBloc

--- edium/lib/presentation/teacher/course_detail/bloc/course_detail_bloc.dart
+++ edium/lib/presentation/teacher/course_detail/bloc/course_detail_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:edium/core/storage/profile_storage.dart';
 import 'package:edium/domain/entities/course_detail.dart';
 import 'package:edium/domain/usecases/course/create_module_usecase.dart';
 import 'package:edium/domain/usecases/course/get_course_detail_usecase.dart';
@@ -8,14 +9,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CourseDetailBloc extends Bloc<CourseDetailEvent, CourseDetailState> {
   final GetCourseDetailUsecase _getCourseDetail;
   final CreateModuleUsecase _createModule;
+  final ProfileStorage _profileStorage;
   final String courseId;
 
   CourseDetailBloc({
     required GetCourseDetailUsecase getCourseDetail,
     required CreateModuleUsecase createModule,
+    required ProfileStorage profileStorage,
     required this.courseId,
   })  : _getCourseDetail = getCourseDetail,
         _createModule = createModule,
+        _profileStorage = profileStorage,
         super(const CourseDetailInitial()) {
     on<LoadCourseDetailEvent>(_onLoad);
     on<CreateModuleEvent>(_onCreateModule);
@@ -29,6 +33,13 @@ class CourseDetailBloc extends Bloc<CourseDetailEvent, CourseDetailState> {
     return null;
   }
 
+  // Intersects the API-reported isTeacher with the current stored role.
+  // Prevents teacher UI from appearing when the user is in student mode.
+  CourseDetail _applyRoleGuard(CourseDetail course) {
+    final isCurrentlyTeacher = _profileStorage.getRole() == 'teacher';
+    return course.copyWith(isTeacher: course.isTeacher && isCurrentlyTeacher);
+  }
+
   Future<void> _onLoad(
     LoadCourseDetailEvent event,
     Emitter<CourseDetailState> emit,
@@ -36,7 +47,7 @@ class CourseDetailBloc extends Bloc<CourseDetailEvent, CourseDetailState> {
     emit(const CourseDetailLoading());
     try {
       final course = await _getCourseDetail(courseId: event.courseId);
-      emit(CourseDetailLoaded(course));
+      emit(CourseDetailLoaded(_applyRoleGuard(course)));
     } catch (e) {
       emit(CourseDetailError(e.toString()));
     }
@@ -51,7 +62,7 @@ class CourseDetailBloc extends Bloc<CourseDetailEvent, CourseDetailState> {
     try {
       await _createModule(courseId: courseId, title: event.title);
       final updated = await _getCourseDetail(courseId: courseId);
-      emit(CourseModuleCreated(updated));
+      emit(CourseModuleCreated(_applyRoleGuard(updated)));
     } catch (e) {
       emit(CourseDetailActionError(e.toString(), current));
     }
