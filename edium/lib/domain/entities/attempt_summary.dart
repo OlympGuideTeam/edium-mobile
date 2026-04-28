db8aac26398ed2affbc8e10a5a4bb3f408d510a8
mmrdr <kialisaev@edu.hse.ru>
2026-04-28 13:08:29 +0300

    Пофиксил UI

--- edium/lib/domain/entities/attempt_summary.dart
+++ edium/lib/domain/entities/attempt_summary.dart
@@ -3,12 +3,14 @@ import 'package:edium/domain/entities/quiz_attempt.dart' show AttemptStatus;
 class AttemptSummary {
   final String attemptId;
   final String userId;
+  final String? userName;
   final AttemptStatus status;
   final double? score;
 
   const AttemptSummary({
     required this.attemptId,
     required this.userId,
+    this.userName,
     required this.status,
     this.score,
   });
