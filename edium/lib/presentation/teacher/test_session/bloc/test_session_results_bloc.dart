f97314ad4989c9361c8bfae4502abb6e5dd8db83
mmrdr <kialisaev@edu.hse.ru>
2026-05-06 01:33:54 +0300

    Переделал UI

--- edium/lib/presentation/teacher/test_session/bloc/test_session_results_bloc.dart
+++ edium/lib/presentation/teacher/test_session/bloc/test_session_results_bloc.dart
@@ -1,6 +1,6 @@
-import 'package:edium/domain/entities/class_detail.dart';
+import 'package:edium/domain/entities/live_session.dart';
 import 'package:edium/domain/entities/quiz_attempt.dart' show AttemptStatus;
-import 'package:edium/domain/repositories/class_repository.dart';
+import 'package:edium/domain/repositories/live_repository.dart';
 import 'package:edium/domain/repositories/test_session_repository.dart';
 import 'package:edium/domain/usecases/test_session/list_session_attempts_usecase.dart';
 import 'package:edium/presentation/teacher/test_session/bloc/test_session_results_event.dart';
@@ -11,23 +11,23 @@ class TestSessionResultsBloc
     extends Bloc<TestSessionResultsEvent, TestSessionResultsState> {
   final ListSessionAttemptsUsecase _listAttempts;
   final ITestSessionRepository _repo;
-  final IClassRepository _classRepo;
+  final ILiveRepository _liveRepo;
 
   String? _sessionId;
-  String? _classId;
+  String? _moduleId;
   String _title = '';
 
   TestSessionResultsBloc({
     required ListSessionAttemptsUsecase listAttempts,
     required ITestSessionRepository repo,
-    required IClassRepository classRepo,
+    required ILiveRepository liveRepo,
   })  : _listAttempts = listAttempts,
         _repo = repo,
-        _classRepo = classRepo,
+        _liveRepo = liveRepo,
         super(const TestSessionResultsInitial()) {
     on<LoadSessionResultsEvent>((e, emit) async {
       _sessionId = e.sessionId;
-      _classId = e.classId;
+      _moduleId = e.moduleId;
       _title = e.title;
       await _load(emit);
     });
@@ -42,20 +42,19 @@ class TestSessionResultsBloc
     if (sid == null) return;
     emit(const TestSessionResultsLoading());
     try {
-      final cid = _classId;
+      final mid = _moduleId;
       final futures = await Future.wait<dynamic>([
         _listAttempts(sid),
-        if (cid != null) _classRepo.getClassDetail(classId: cid),
+        if (mid != null) _liveRepo.getModuleRoster(mid),
       ]);
 
       final attempts = futures[0] as List;
-      final classDetail = (cid != null && futures.length > 1)
-          ? futures[1] as ClassDetail
-          : null;
+      final rosterMembers = (mid != null && futures.length > 1)
+          ? futures[1] as List<LiveRosterMember>
+          : <LiveRosterMember>[];
 
       final nameMap = {
-        for (final s in classDetail?.students ?? <MemberShort>[])
-          s.id: s.fullName,
+        for (final m in rosterMembers) m.userId: m.name,
       };
 
       final rows = attempts
